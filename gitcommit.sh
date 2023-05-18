91e13b31d8b0b1141a02248fb4ab36aff9ad1727
Luan Santos <lvluanvinicius@outlook.com.br>
2023-05-18 09:31:46 -0300

    Atualizando repositório novamente

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clear
-echo -e "Realizando commit do respositório."
+echo -e "\nRealizando commit do respositório.\n"
 git add .
 git commit -m "$1"
 eval "$(ssh-agent -s)" && ssh-add ~/.ssh/analytics
