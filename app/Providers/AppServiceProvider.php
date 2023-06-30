c74b23e44334a5da4a6dfa2d1ed216ecfa1c966c
Grupo CedNet <devel@cednet.com.br>
2023-06-30 10:00:51 -0300

    Atualizando ratelimit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(150)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(1000);
         });
     }
 }
