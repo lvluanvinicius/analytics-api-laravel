b1289ee1cacbfa15bf3e7370e719cca962d73ad5
Grupo CedNet <devel@cednet.com.br>
2023-08-23 12:45:04 -0300

    Finalizada listagem de triggers com localizações

--- app/Http/Controllers/Api/PmOurinhosController.php
+++ app/Http/Controllers/Api/PmOurinhosController.php
@@ -17,49 +17,72 @@ class PmOurinhosController extends Controller
     {
         try {
             // Carregando localizações.
-        $location_data = json_decode(file_get_contents(storage_path('app/locales/location.json')));
-        
-        // Criando modelo de conexão com Zabbix Api
-        $zabbixLocales = new LocalesTriggers(
-            $urlbase=$request->get('zabbixlocation'),
-            $token=$request->headers->get('zabbixtoken')
-        );
-        
-        // Buscando dados.
-        $zabbixData = $zabbixLocales->request([
-            "jsonrpc" => "2.0",
-            "method" => "item.get",
-            "id" => 1,
-            "params" => [
-                "output" => ["extends"],
-                "filter" => [
-                    "groupids" => [config('zabbix.groups.pm_our')],
-                ],
-                "search" => [
-                    "key_" => "ifOnuOperStatus",
-                    "name" => "PMO"
-                ],
-                "selectTags" => [
-                    "tag", "value",
-                ],
-                "selectTriggers" => [
-                    "triggers", "description", "comments", "status",
-                ],
-            ],
-        ]);
+            $location_data = json_decode(file_get_contents(storage_path('app/locales/location.json')));
 
-        // Novo array para retorno com os dados separados.
-        $newZabbixData = [];
-        foreach ($zabbixData['result'] as $zbxData) {
-            array_push($newZabbixData, [
-                "itemid" => $zbxData['itemid'],
-                "triggers" => $zbxData['triggers'],
-                "lastvalue" => $zbxData["tags"]
+            // Criando modelo de conexão com Zabbix Api
+            $zabbixLocales = new LocalesTriggers(
+                $urlbase = $request->get('zabbixlocation'),
+                $token = $request->headers->get('zabbixtoken')
+            );
+
+            // Buscando dados.
+            $zabbixData = $zabbixLocales->request([
+                "jsonrpc" => "2.0",
+                "method" => "item.get",
+                "id" => 1,
+                "params" => [
+                    "output" => ["triggers", "lastvalue"],
+                    "filter" => [
+                        "groupids" => [config('zabbix.groups.pm_our')],
+                    ],
+                    "search" => [
+                        "key_" => "ifOnuOperStatus",
+                        "name" => "PMO"
+                    ],
+                    "selectTags" => [
+                        "tag", "value",
+                    ],
+                    "selectTriggers" => [
+                        "triggers", "description", "comments", "status",
+                    ],
+                ],
             ]);
-        }
 
-        // return $this->succes
-        return $this->success($newZabbixData);
+            function arrayFilter(array $locales, array $zbxData)
+            {
+                foreach ($locales as $lc) {
+                    dd($zbxData['triggers']);
+                    if (trim($zbxData['tags'][1]['value']) == trim($lc->name)) {
+                        return $zbxData;
+                    }
+                }
+            }
+
+            // Novo array para retorno com os dados separados.
+            $newZabbixData = [];
+            foreach ($zabbixData['result'] as $zbxData) {
+                $trigger_name = explode(' ', $zbxData['triggers'][0]['description'])[2];
+                $status = $zbxData['lastvalue'];
+
+                foreach ($location_data as $lc) {
+                    // return $lc;
+                    if (trim($trigger_name) == $lc->key) {
+                        array_push($newZabbixData, [
+                            "name" => $lc->name,
+                            "itemid" => $zbxData['itemid'],
+                            "triggers" => $zbxData['triggers'],
+                            "lastvalue" =>  $status,
+                            "locale" => [
+                                $lc->lat,
+                                $lc->lon,
+                            ],
+                        ]);
+                    }
+                }
+            }
+
+            // return $this->succes
+            return $this->success($newZabbixData);
         } catch (Exception $error) {
             return $this->error($error->getMessage());
         }
