3dbaf954cf18262f6db5ffedbaca2a1e6a03ef2a
Grupo CedNet <lvluansantos@gmail.com>
2024-04-05 13:47:49 -0300

    Atualizado rotas de api: Criado recurso de coleta de dados por porta, pegando sempre a ultima coleta.

--- app/Http/Controllers/Api/GponOnusPerPortsController.php
+++ app/Http/Controllers/Api/GponOnusPerPortsController.php
@@ -62,16 +62,29 @@ class GponOnusPerPortsController extends Controller
 
         $params = $request->query();
 
+
         // Recuperando timerange
         $equipament = $params["equipament"];
         $port = $params["port"];
-        $collection_date = (new \DateTime())->modify('-1 hour')->format('Y-m-d H:i:s');
+        $timeFromString = (new \DateTime())->modify('-1 hour')->format('Y-m-d H:i:s'); // (new \DateTime())->format('Y-m-d H:i:s');
+        $timeToString = (new \DateTime())->format('Y-m-d H:i:s');
 
         $onus = new GponOnus();
 
+        // Recuperando ultima data de coleta.
+        $collection_date = $onus->where('device', '=', $equipament)
+            ->where('port', '=', $port)
+            ->where('collection_date', '>=', $timeFromString)
+            ->where('collection_date', '<=', $timeToString)
+            ->orderBy('collection_date', 'desc')
+            ->select('collection_date')
+            ->distinct('collection_date')
+            ->first('collection_date');
+
+        // Recuperando dados na data de coleta.
         $onusData = $onus->where('device', '=', $equipament)
             ->where('port', '=', $port)
-            ->where('collection_date', '=', $collection_date)
+            ->where('collection_date', '=', $collection_date->collection_date)
             ->orderBy('name', 'asc')
             ->get(['onuid', 'serial_number', 'name', 'tx', 'rx', 'device', 'port']);
 
